0693748382137a67d8e04b46473d0a33a635ed5f
MEtooHARD <polar940301@gmail.com>
2024-03-26 09:48:11 +0800

    gg

---
+++ w3/w3q3.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class SquareMatrix {
+    private:
+    int size = 0;
+    int *matrix;
+
+    public:
+        
+};
+
+int main() {
+    bool readingStatus = false;
+}
